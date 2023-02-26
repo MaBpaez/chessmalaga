f2cad2217def2fe4cdbdff53525f377d814a44d4
manuel <manuel@manuel.home>
2023-02-26 08:54:20 +0100

    Added cloudinary

--- ajedrez/settings.py
+++ ajedrez/settings.py
@@ -186,3 +186,12 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cloudinary
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('API_KEY'),
+    'API_SECRET': env('API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
